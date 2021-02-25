71f41a4a979b44ebb0253fc68aa6f514c7e49829
Artem <nik.artem.larin@gmail.com>
2021-02-25 14:41:19 +0600

    добавил FamilyNames

---
+++ 001_beonmax_Java/001_FamilyNames/src/FamilyNames.java
@@ -0,0 +1,8 @@
+public class FamilyNames {
+    public static void main(String... args){
+        System.out.println("Виктория");
+        System.out.println("Максим");
+        System.out.println("Александр");
+        System.out.println("Денис");
+    }
+}
